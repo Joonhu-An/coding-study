354e866f303ee5002584c95c84cb8c497d7b9d65
joonhu <wnsgn0913@naver.com>
2025-08-28 15:55:36 +0900

    coyote update

---
+++ magic_shark_fireball_practice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M, K;
+
+struct Fire{
+    int r, c, m, s, d;
+};
+
+vector<Fire> fvect;
+
+/*  freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    */
